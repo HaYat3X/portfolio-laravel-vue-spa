3cb8533b3e92664d08f4b64bcae3ae15aaede35e
Hayate_T <hayatesato1@gmail.com>
2023-10-06 22:55:34 +0900

    環境構築

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Hello World</h1>
+    <v-app-bar :elevation="2"></v-app-bar>
+    <v-icon icon="mdi-account" class="ma-4" />
+</template>
+
+<style>
+h1 {
+    text-align: center;
+}
+</style>
